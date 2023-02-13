5ed9587e306f086816042602471ec2546060cb5a
Rafael <grafa74@gmail.com>
2023-02-13 12:46:37 +0100

    added user repo and actiontypes

---
+++ src/services/user/actionTypes.ts
@@ -0,0 +1,15 @@
+export const productActionTypes = {
+    add: "products@add",
+    update: "products@update",
+    load: "products@load",
+    select: "products@select",
+};
+
+export const userActionTypes = {
+    startLogin: "users@startLogin",
+    login: "users@login",
+    logout: "users@logout",
+    addFavorites: "users@addFavorites",
+    removeFavorites: "users@removeFavorites",
+    requestProduct: "users@requestProduct",
+};
